8dc8b5439f5be1756128c718efa20be6b9524918
casjora <casjora@gmail.com>
2026-06-22 17:07:26 -0600

    Logica de balance e Historial completada y probada

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 
 export default function Balance({total,income,expense}) {
+
+    /* Primer intento, pero no necesario si uso props desde el App.jsx:
     const [balance,setBalance]=useState(0.00)
     const [incomeBalance,setIncomeBalance]=useState(0)
-    const [expenseBalance,setExpenseBalance]=useState(0)
+    const [expenseBalance,setExpenseBalance]=useState(0) */
 
     
   return (
@@ -11,15 +13,15 @@ export default function Balance({total,income,expense}) {
         <h1 className="text-2xl font-bold mt-10">React Wallet</h1>
         <div className="my-8 flex flex-col w-full justify-center items-center">
          <p className=" text-sm text-slate-400 font-bold block md:hidden">TU BALANCE</p>
-         <em className="not-italic text-6xl font-bold">${balance.toFixed(2)}</em>
+         <em className="not-italic text-6xl font-bold">${total < 0 ? '-':''}{Math.abs(total).toFixed(2)}</em>
          <div className="mt-5 w-7/10 md:flex hidden ">
             <div className="border-r-2 border-r-slate-200 w-1/2 flex flex-col justify-center items-center" >
                 <p className="text-slate-400 font-bold">INCOME</p>
-                <span className="text-emerald-600 font-bold">+${incomeBalance.toFixed(2)}</span>
+                <span className="text-emerald-600 font-bold">+${income.toFixed(2)}</span>
             </div>
             <div className=" w-1/2 flex flex-col justify-center items-center">
                 <p className=" text-slate-400 font-bold">EXPENSE</p>
-                <span className="text-red-600 font-bold">-${expenseBalance.toFixed(2)}</span>
+                <span className="text-red-600 font-bold">-${expense.toFixed(2)}</span>
             </div>
          </div>
         </div>
--- src/components/Historial.jsx
+++ src/components/Historial.jsx
@@ -1,14 +1,37 @@
 import { useState } from "react"
 
-export default function Historial() {
+export default function Historial({transactions,deleteTransaction}) {
     
   return (
     <div className="mt-8">
         <h2 className="pb-2 border-b-2 border-b-slate-300 text-sm font-medium">Historial</h2>
-        <ul>
+        {transactions.length ===0 ? (
+          <p>
+            No hay movimientos. Agrega uno!
+          </p>
+        ): (
+          <ul>
+            {transactions.map((t)=>{
+              const esPositivo = t.amount > 0
+              console.log(`${t} - ${t}`)
+              return(
+                <li key={t.id}>
+                  <div>
+                    <button onClick={()=>deleteTransaction(t.id) }>X</button>
+                    <span>{t.text}</span>
+                  </div>
+                  <span className={`font-bold text-sm ${esPositivo ? 'text-emerald-500':'text-red-500'}`}>
+                    {esPositivo ? '+' : ''}${t.amount.toFixed(2)}
+                  </span>
+                </li>
+              )
+            })}
+          </ul>
+        )}
+        {/* <ul>
             <li className="flex justify-between px-2 w-full border">{`Pago Nomina`} <span className="text-emerald-500 font-bold">+${500.00.toFixed(2)}</span></li>
             <li className="flex justify-between px-2 w-full border">{`Compra Pizza`} <span className="text-red-500 font-bold">-${20.00.toFixed(2)}</span></li>
-        </ul>
+        </ul> */}
 
       
     </div>
